51c9060c04943afe5a94680632f30f044e6714a0
c0d3l0v3r-HeHe <codelover2404@gmail.com>
2026-03-16 01:24:11 +0530

    removed redundant code and improved UI

--- dashboard-next/app/(auth)/login/page.tsx
+++ dashboard-next/app/(auth)/login/page.tsx
@@ -272,15 +272,7 @@ export default function LoginPage() {
             </motion.button>
           </form>
 
-          {/* Divider */}
-          <div className="relative my-6">
-            <div className="absolute inset-0 flex items-center">
-              <div className="w-full border-t border-white/[0.06]" />
-            </div>
-            <div className="relative flex justify-center">
-              <span className="bg-transparent px-3 text-zinc-600 text-xs">or continue with</span>
-            </div>
-          </div>
+        
 
 
         </motion.div>
